0e390ca5ef62eba7b39fd34ef42efbd7fd3d4351
floghos <caleuche94@gmail.com>
2023-04-25 13:16:11 -0400

    env renamed to MyEnv

---
+++ Prototypes/ControlSystem_v1/MyEnv_temp.py
@@ -0,0 +1,7 @@
+# Environment variables
+
+#ROTOR_IP = "xxx.xxx.xxx.xxx" # STRING
+#ROTOR_PORT = xxxx # INT
+#TRACKER_IP "xxx.xxx.xxx.xxx" # STRING
+#TRACKER_PORT = xxxx # INT
+#CONTROLLER_SERIAL_PORT = "XXXX" # STRING
--- Prototypes/ControlSystem_v1/env_temp.py
+++
@@ -1,4 +0,0 @@
-# Environment variables
-
-#ROTOR_IP = "xxx.xxx.xxx.xxx" (str)
-#ROTOR_PORT = port (int)
