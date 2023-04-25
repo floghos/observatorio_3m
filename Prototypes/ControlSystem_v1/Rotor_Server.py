6107882c3f3dfae7b78f7606b92af275dced5d17
floghos <caleuche94@gmail.com>
2023-04-25 16:14:03 -0400

    getCurrentPos() now formats data before returning

--- Prototypes/ControlSystem_v1/Rotor_Server.py
+++ Prototypes/ControlSystem_v1/Rotor_Server.py
@@ -3,44 +3,55 @@ import traceback
 import Ice
 import RotorModule
 import serial
-from env.py import *
-
-
+import MyEnv
 
 
 class RotorI(RotorModule.Rotor):
     # Currently using YAESU protocol. Planning to change to Rot2Prog eventually.
 
-    def gotoAziAlt(self, azi, alt, current=None):
-        #ser.reset_input_buffer()
-        # The output of the serial.write() method is an integer representing the number of bytes written to the serial port
+    def gotoAziAlt(self, azi, alt, current=None) -> None:
         azi = int(azi)
         alt = int(alt)
         command = f'W{azi:03d} {alt:03d}\r\n'
         print(command)
         
-        #ser.write(command.encode('UTF-8'))
+        ser.reset_input_buffer()
+        ser.write(command.encode('UTF-8'))
+        # The output of the serial.write() method is an integer representing the number of bytes written to the serial port
         
         #Reading response from serial port
-        #response = ser.readline().decode().strip()
+        response = ser.readline().decode().strip()
         #print(f"Response: {response}")
 
-    def getCurrentPos(self, current=None):
+    def getCurrentPos(self, current=None) -> str:
         command = f'C2\r\n'
-        #ser.reset_input_buffer()
-        #ser.write(command.encode('UTF-8'))
+        # The C2 command returns current rotor position in as: "AZ=aaa EL=eee"
 
-        #pos = ser.readline().decode().strip()
+        ser.reset_input_buffer()
+        ser.write(command.encode('UTF-8'))
+        res = ser.readline().decode().strip()
+
+        s = res.split()
+        s[0] = s[0].strip('AZ=')
+        s[1] = s[1].strip('EL=')
+        pos = s[0] + " " + s[1]
+
         return pos
+    
+    def stop(self, current=None) -> None:
+        command = f'S\r\n'
+        ser.reset_input_buffer()
+        ser.write(command.encode('UTF-8'))
+        pass
 
 
 status = 0
 ic = None
 try:
-    #ser = serial.Serial('COM3', 9600, timeout=5)
+    ser = serial.Serial('COM6', 9600, timeout=5)
 
     ic = Ice.initialize(sys.argv)
-    adapter = ic.createObjectAdapterWithEndpoints("SimpleRotorAdapter", "default -p 10001")
+    adapter = ic.createObjectAdapterWithEndpoints("SimpleRotorAdapter", f"default -p {MyEnv.ROTOR_PORT}")
     object = RotorI()
     adapter.add(object, ic.stringToIdentity("SimpleRotor"))
     adapter.activate()
@@ -55,7 +66,7 @@ if ic:
     try:
         ic.destroy()
         # Closing the serial connection
-        #ser.close()
+        ser.close()
     except:
         traceback.print_exc()
         status = 1
