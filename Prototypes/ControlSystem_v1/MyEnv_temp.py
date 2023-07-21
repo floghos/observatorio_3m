06997781cbcb6b6ddb820744998ace32e59920b4
floghos <caleuche94@gmail.com>
2023-07-21 11:24:34 -0400

    More details for MyEnv-temp.py

--- Prototypes/ControlSystem_v1/MyEnv_temp.py
+++ Prototypes/ControlSystem_v1/MyEnv_temp.py
@@ -1,8 +1,10 @@
 # Environment variables
+# Uncomment and set the ones needed to work on your environment.
+# Usually, attempting to run without setting all the required variables will throw an error indicating the missing ones, so just go for it.
 
 #ROTOR_IP = "xxx.xxx.xxx.xxx" # STRING (IP of the PC hosting the Rotor Server)
 #ROTOR_PORT = xxxx # INT (Port that the ROTOR Server will be using to communicate through Ice)
-#TRACKER_IP "xxx.xxx.xxx.xxx" # STRING (IP of the PC hosting the Tracking Server)(Running Tracking_server.py locally does not require providing an IP)
+#TRACKER_IP = "xxx.xxx.xxx.xxx" # STRING (IP of the PC hosting the Tracking Server)(Set to "" if you run the tracking server locally)
 #TRACKER_PORT = xxxx # INT (Port that the TRACKING Server will be using to communicate through Ice)
 #CONTROLLER_SERIAL_PORT = "XXXX" # STRING (Name of the port the PC running the ROTOR server uses to communicate with the rotor controller. E.g.: COM1, COM2...)
 #STEL_RC_PORT = xxxx # INT (Stellarium Remote-Control plugin's port. Set both, this and the plugin's port, to match)
